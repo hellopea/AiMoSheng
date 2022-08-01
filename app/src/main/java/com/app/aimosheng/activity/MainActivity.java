83cbaf0fcde5c96700be5bc27c6594149db10d0e
hellopea <822079950@qq.com>
2022-08-01 23:00:06 +0800

    add main and menu ui

--- app/src/main/java/com/app/aimosheng/activity/MainActivity.java
+++ app/src/main/java/com/app/aimosheng/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.app.aimosheng;
+package com.app.aimosheng.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.app.aimosheng.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/app/aimosheng/activity/MenuActivity.java
@@ -0,0 +1,22 @@
+package com.app.aimosheng.activity;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.app.aimosheng.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_menu);
+    }
+}
